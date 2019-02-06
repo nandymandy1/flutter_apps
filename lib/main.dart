b9190a38ac6a3097d48f6246fd8c6e6b7ffdfc25
Narendra Maurya <nandymandy09@hotmail.com>
2019-02-06 19:09:32 +0530

    Structure of the app re assigned

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import './chatApp/myChatApp.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: 'My Apps', home: new ChatApp());
+    return new MaterialApp(title: 'My Apps');
   }
 }
